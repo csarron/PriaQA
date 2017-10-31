752d0ab33499d807f24baf1aecb41eee33f4f438
Arron Cao <qqcao.cs@gmail.com>
2017-10-31 15:37:05 -0400

    add: galago search

--- src/info/ephyra/search/searchers/GalagoKM.java
+++ src/info/ephyra/search/searchers/GalagoKM.java
@@ -1,4 +1,27 @@
 package info.ephyra.search.searchers;
 
-public class GalagoKM {
+import info.ephyra.search.Result;
+
+public class GalagoKM extends KnowledgeMiner {
+
+
+    @Override
+    protected int getMaxResultsTotal() {
+        return 0;
+    }
+
+    @Override
+    protected int getMaxResultsPerQuery() {
+        return 0;
+    }
+
+    @Override
+    public KnowledgeMiner getCopy() {
+        return null;
+    }
+
+    @Override
+    protected Result[] doSearch() {
+        return new Result[0];
+    }
 }
